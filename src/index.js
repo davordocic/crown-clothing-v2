91161165262448fd72b728eb31ba1345cf0fee8c
davor.docic <davor.docic@koncar.hr>
2023-08-29 13:06:22 +0200

    Context continued 114

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
 
 import "./index.scss";
 
 const rootElement = document.getElementById("root");
 
 render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+	<React.StrictMode>
+		<BrowserRouter>
+			<UserProvider>
+				<ProductsProvider>
+					<App />
+				</ProductsProvider>
+			</UserProvider>
+		</BrowserRouter>
+	</React.StrictMode>,
+	rootElement
 );
