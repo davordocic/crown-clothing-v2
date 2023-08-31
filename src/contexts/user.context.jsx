7b613e721bd724246774189c9c5f1e40ad216037
davor.docic <davor.docic@koncar.hr>
2023-08-31 14:19:14 +0200

    UserReducer done

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,29 +1,58 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
+	onAuthStateChangedListener,
+	createUserDocumentFromAuth,
 } from '../utils/firebase/firebase.utils';
+import { act } from 'react-dom/test-utils';
 
 export const UserContext = createContext({
-  setCurrentUser: () => null,
-  currentUser: null,
+	setCurrentUser: () => null,
+	currentUser: null,
 });
 
+
+export const USER_ACTION_TYPES = {
+	SET_CURRENT_USER: "SET_CURRENT_USER"
+};
+
+const UserReducer = (state, action) => {
+	const { type, payload } = action;
+
+	switch(type) {
+		case USER_ACTION_TYPES.SET_CURRENT_USER:
+			return {
+				...state,
+				currentUser: payload
+			};
+		default:
+			throw new Error(`Unhandled type ${type} in userReducer.`);
+	};
+};
+
+const INITIAL_STATE = {
+	currentUser: null
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+	const [ { currentUser }, dispatch] = useReducer(UserReducer, INITIAL_STATE);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-      setCurrentUser(user);
-    });
+	const setCurrentUser = (user) => {
+		dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+	};
 
-    return unsubscribe;
-  }, []);
+	const value = { currentUser, setCurrentUser };
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedListener((user) => {
+			if (user) {
+				createUserDocumentFromAuth(user);
+			}
+			setCurrentUser(user);
+		});
+
+		return unsubscribe;
+	}, []);
+
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
